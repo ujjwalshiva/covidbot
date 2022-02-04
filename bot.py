a1a29ee09d165fce39942646f21270ba3db57ee8
Ujjwal Acharya <81429137+ujjwalshiva@users.noreply.github.com>
2022-02-04 17:05:45 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+# Required Modules
+
 import requests
 import telebot
 from bs4 import BeautifulSoup
@@ -5,25 +7,34 @@ import sys
 import io
 
 
+#API Key Details
+
 API_KEY = "Your API Key Here"
 bot = telebot.TeleBot(API_KEY)
+
+#Required Header JSON
 
 h = {
     "Cache-Control": "no-cache",
     "Pragma": "no-cache"
 }
 
+#Start Command
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hey there! How are you!,\nType /latest to get latest Telanagana Covid figures.\nType /help to know more \nType /cases <state-name> to get Covid cases data \nType /jntu to get latest latest JNTU Circular \n Type /india to get latest Covid News')
 
 
+#Help Command
+    
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.reply_to(message, "Hi There! This is a simple bot to check the current COVID data of Telangana by displaying real time data from Times Of India. Type /start to get started. Built by @ujjwalshiva")
 
 
+#India Command
+    
 @bot.message_handler(commands=['india'])
 def india(message):
     url = "https://timesofindia.indiatimes.com/coronavirus"
@@ -33,10 +44,13 @@ def india(message):
     bot.reply_to(message, "Latest TOI Covid News"+'\n'+news)
 
 
+#Latest Command
+    
 @bot.message_handler(commands=['latest'])
 def latest(message):
     bot.reply_to(message, "Fetching latest COVID Telangana Figures")
-    
+
+    #Saves STDOUT as string
     old_stdout = sys.stdout
     new_stdout = io.StringIO()
     sys.stdout = new_stdout
@@ -48,6 +62,7 @@ def latest(message):
 
     soup = BeautifulSoup(text, 'html.parser')
 
+    #Fetching Date from Telangana Health Ministry
     datehtml = soup.findAll('font')
     for i in datehtml:
         if ':' in i.get_text():
@@ -56,6 +71,7 @@ def latest(message):
     print(f"As of *{date}*")
     print()
 
+    #Fetching Cases Count
     caseshtml = soup.find('tbody')
     cases = caseshtml.findAll('b')
 
@@ -70,10 +86,13 @@ def latest(message):
     print()
     print('* Data fetched from Telangana Health Ministry *')
 
+    #Output Final message
     output = new_stdout.getvalue()
     sys.stdout = old_stdout
     bot.send_message(message.chat.id,output)
 
+#JNTU Command
+    
 @bot.message_handler(commands=['jntu'])
 def jntu(message):
     bot.reply_to(message, "Latest Post from JNTU Bulletin Board")
@@ -82,7 +101,7 @@ def jntu(message):
     text = r.text
 
     soup = BeautifulSoup(text, 'html.parser')
-
+    #Fetching data by row
     status = soup.find('tr',class_='griderow1')
     new = status.find_all('td')
     link = status.find('a', href=True)['href']
@@ -90,6 +109,7 @@ def jntu(message):
     new = [i.text.strip() for i in new]
     #print(new[1],': ', new[2])
 
+    #URL to send Document in Telegram
     url = "https://api.telegram.org/bot<API_KEY>/sendDocument"
 
     payload = {
@@ -107,6 +127,8 @@ def jntu(message):
     response = requests.request("POST", url, json=payload, headers=headers)
 
 
+#Cases Command
+    
 @bot.message_handler(commands=['cases'])
 def cases(message):
     query = message.text.replace('/cases ', '')
@@ -115,6 +137,7 @@ def cases(message):
     if key=='Kerala':
         key = 'Kerala***'
 
+    #Base URL for COVID India API
     url = "https://api.rootnet.in/covid19-in/stats/latest"
     r = requests.get(url, headers=h).json()
     data = r["data"]["regional"]
@@ -137,5 +160,5 @@ def cases(message):
         output = "Please enter proper state name with correct spelling"
         bot.reply_to(message, output)
 
-
+#Required command to keep Bot running
 bot.polling()
